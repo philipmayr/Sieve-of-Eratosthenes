560ee96b2f73306827e170221c7ef6be12614adf
Phil Samuel <philipmayr@outlook.com>
2024-10-06 20:38:26 -0500

    Update algorithm.c

--- algorithm.c
+++ algorithm.c
@@ -1,55 +1,32 @@
 // Sieve of Eratosthenes
 
 #include <stdio.h>
-#include <stdlib.h>
-
-typedef struct Node
-{
-    int value;
-    
-    struct Node* next_node_pointer;
-} Node;
-
-Node* create_node(int value)
-{
-    Node* new_node = (Node*) malloc(sizeof(Node));
-    
-    new_node->value = value;
-    new_node->next_node_pointer = NULL;
-    
-    return new_node;
-}
-
-void append(struct Node** head_node, int value)
-{
-    Node* new_node = (Node*) malloc(sizeof(Node));
-    Node* last_node = *head_node;
-    
-    new_node->value = value;
-    new_node->next_node_pointer = NULL;
-    
-    if (*head_node == NULL)
-    {
-        *head_node = new_node;
-        
-        return;
-    }
-    else
-    {
-        while (last_node->next_node_pointer != NULL)
-        {
-            last_node = last_node->next_node_pointer;
-        }
-        
-        last_node->next_node_pointer = new_node;
-        
-        return;
-    }
-}
+#include <string.h>
 
 int main()
 {
-    Node* first = create_node(2);
+    int upper_bound = 100;
+    int lower_bound = 2;
+    
+    int sieve[upper_bound];
+    
+    memset(sieve, 0, sizeof(sieve));
+    
+    for (int index = lower_bound; index <= upper_bound; index++)
+    {
+        if (sieve[index]) continue;
+        
+        for (int multiple = index * 2; multiple <= upper_bound; multiple += index)
+        {
+            sieve[multiple] = 1;
+        }
+    }
+    
+    // primes are indicated by 0's, composites by 1's
+    for (int index = lower_bound; index <= upper_bound; index++)
+    {
+        printf("%d ", sieve[index]);
+    }
     
     return 0;
 }
